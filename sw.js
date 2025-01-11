3f17f56cc0a61b8454a2c4b07c440e485e460636
asap-controls@hotmail.com <asap-controls@hotmail.com>
2025-01-11 14:17:45 +0000

    Added all content to main page

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 var GHPATH = '/SD-Calculator';
 var APP_PREFIX = 'SD Calculator_';
-var VERSION = 'version_004';
+var VERSION = 'version_005';
 var URLS = [    
   `${GHPATH}/`,
   `${GHPATH}/index.html`,
