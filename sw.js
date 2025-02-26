00e74c7d9d71612ee10c140b0d185c1eadc01ec9
asap-controls@hotmail.com <asap-controls@hotmail.com>
2025-02-26 12:28:34 +0000

    Updated save/load function to fix bugs found in deployment, added status bar and fixed some layout issues

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 var GHPATH = '/SD-Calculator';
 var APP_PREFIX = 'SD Calculator_';
-var VERSION = 'version_008';
+var VERSION = 'version_009';
 var URLS = [    
   `${GHPATH}/`,
   `${GHPATH}/index.html`,
